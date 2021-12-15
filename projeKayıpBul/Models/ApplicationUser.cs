06420911d647e09a51e146bd9d16baf1550e8b1c
Barış <55881250+barisern@users.noreply.github.com>
2021-12-15 21:15:49 +0300

    Initial Commit - Tema giydirilmesi

---
+++ projeKayıpBul/Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeKayıpBul.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [NotMapped]
+        public string NameSurname
+        {
+            get
+            {
+                return $"{Name} {Surname}";
+            }
+        }
+    }
+}
