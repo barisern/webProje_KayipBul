43ccccc5b244db2415a5af23faaa671d0cff2d70
Barış <55881250+barisern@users.noreply.github.com>
2021-12-19 23:01:57 +0300

    Add more model

---
+++ projeKayıpBul/Models/Lost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeKayıpBul.Models
+{
+    public class Lost
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
